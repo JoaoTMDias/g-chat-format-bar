171fc6336b898c60a10b608a6c30391093972c71
João Dias <joao.dias@feedzai.com>
2020-04-20 00:30:56 +0100

    can query all textboxes and attach an widget

--- src/contentscript/contentscript.ts
+++ src/contentscript/contentscript.ts
@@ -1,18 +1,1 @@
 import './contentscript.scss';
-
-const isThisContentscript = true;
-console.log('isThisContentscript', isThisContentscript);
-
-document.addEventListener("DOMContentLoaded", () => {
-    const messageContainer = document.querySelector("[role='main']");
-
-    const allBoxes = Array.from(messageContainer?.querySelectorAll("[role='textbox']"));
-
-    if (allBoxes) {
-        const lastChild = allBoxes[allBoxes.length - 1];
-        const node = document.createElement("div");                 // Create a <li> node
-        const textnode = document.createTextNode("test");         // Create a text node
-        node.appendChild(textnode);                              // Append the text to <li>
-        lastChild.appendChild(node)
-    }
-});
--- src/popup/src/App.tsx
+++ src/popup/src/App.tsx
@@ -1,26 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { List } from "./components/list";
 import { MessageController } from "./context/message-controller";
 import { IconsList } from "./data/icons";
 import "./styles/index.scss";
-import DummyTextbox from "./components/dummy";
+import { ITextbox } from "index";
 
-export default function App() {
+interface IAppProps {
+  items: ITextbox[];
+}
+
+export default function App({ items }: IAppProps) {
+  console.log("items: ", items);
+
+  useEffect(() => {
+    if (items?.length > 0) {
+      items.forEach((item) => {
+        item.input.addEventListener("focus", () => {
+          console.log("recebeu foco");
+        });
+      })
+    }
+  })
+
   return (
-    <div>
-      <DummyTextbox />
-      <MessageController>
-        <div
-          role="toolbar"
-          aria-label="Text Formatting"
-          aria-controls=""
-          className="g-chat-format-bar"
-        >
-          <div className="g-chat-format-bar__wrapper">
-            <List list={IconsList} />
-          </div>
+    <MessageController>
+      <div
+        role="toolbar"
+        aria-label="Text Formatting"
+        aria-controls=""
+        className="g-chat-format-bar"
+      >
+        <div className="g-chat-format-bar__wrapper">
+          <List list={IconsList} />
         </div>
-      </MessageController>
-    </div>
+      </div>
+    </MessageController>
   );
 }
